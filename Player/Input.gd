bec831780226c0ebfdc4c2e13b4997b6dc1baf77
hamzasari <hamza.sari@gmail.com>
2026-02-03 20:35:20 +0000

    refactor: comment out processing for 'hit', 'roll', and 'dash' input actions.

--- Player/Input.gd
+++ Player/Input.gd
@@ -7,14 +7,14 @@ func gather_input() -> InputPackage:
 	if Input.is_action_just_pressed("ui_accept"):
 		new_input.actions.append("jump")
 	
-	if Input.is_action_just_pressed("hit"):
-		new_input.actions.append("hit")
+	# if Input.is_action_just_pressed("hit"):
+	# 	new_input.actions.append("hit")
 	
-	if Input.is_action_just_pressed("roll"):
-		new_input.actions.append("roll")
+	# if Input.is_action_just_pressed("roll"):
+	# 	new_input.actions.append("roll")
 		
-	if Input.is_action_just_pressed("dash"):
-		new_input.actions.append("dash")
+	# if Input.is_action_just_pressed("dash"):
+	# 	new_input.actions.append("dash")
 	
 	new_input.input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if new_input.input_direction != Vector2.ZERO:
